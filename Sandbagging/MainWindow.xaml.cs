4a89cb647154048562fb5627a281b5b03d788a02
Jan01 <Klyne000@yahoo.com>
2014-04-21 15:54:27 +0200

    Added drag/drop to GUI

--- Sandbagging/MainWindow.xaml.cs
+++ Sandbagging/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Xml.Linq;
 
 namespace Sandbagging
@@ -24,6 +17,18 @@ namespace Sandbagging
         public MainWindow()
         {
             InitializeComponent();
+            blocks = LogTextBox.Document.Blocks;
+            blocks.Clear();
+            Paragraph para = CreatePara("Sandbagging");
+            para.FontWeight = FontWeights.Bold;
+            para.FontSize = 18;
+            blocks.Add(para);
+            AddPara("Drag XML Documentation file(s) to convert here.");
+            AddPara("Check your Visual Studio/project properties/build tab, to see where the XML Documentation file is generated.");
+            AddPara("Useful links:");
+            //LogTextBox.AppendText("Useful links:");
+            AddLink("https://github.com/Jan01/Sandbagging", "Sandbagging");
+            AddLink("http://www.helpndoc.com", "HelpNDoc");
         }
         private Dictionary<string, DocMember> members;
         private void LoadDoc(string fileName)
@@ -96,21 +101,21 @@ namespace Sandbagging
             string[] parts = qualifiedName.Split('.');
             return parts.Last();
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void GenerateHtml(string rootPath)
         {
-            members = new Dictionary<string, DocMember>();
-            LoadDoc(@"C:\Users\Jan\Documents\GitHub\Sandbagging\Sandbagging\bin\Debug\Sandbagging.XML");
-            XElement xml = GenerateHtml();
-            xml.Save(@"C:\Users\Jan\Documents\GitHub\Sandbagging\Sandbagging\bin\Debug\Sandbagging.html");
-        }
-        private XElement GenerateHtml()
-        {
-            XElement html = new XElement("html");
-            XElement body = new XElement("body");
-            html.Add(body);
-            XElement style = new XElement("style");
-            body.Add(style);
-            style.Value = @"body {
+            List<string> generateFileNames = new List<string>();
+            foreach (KeyValuePair<string, DocMember> pair in members)
+            {
+                DocMember member = pair.Value;
+                if (member.DocType == DocTypeEnum.Type)
+                {
+                    string qualifiedTypeName = pair.Key;
+                    XElement html = new XElement("html");
+                    XElement body = new XElement("body");
+                    html.Add(body);
+                    XElement style = new XElement("style");
+                    body.Add(style);
+                    style.Value = @"body {
   font-family:Arial;
 }
 table {
@@ -131,20 +136,47 @@ h1 {
   background-color: #207245;
   color: #FFFFFF;
 }";
-            foreach (KeyValuePair<string, DocMember> member in members)
-            {
-                if (member.Value.DocType == DocTypeEnum.Type)
-                {
-                    string qualifiedTypeName = member.Key;
                     body.Add(new XElement("h1", qualifiedTypeName));
-                    AddChildren(body, DocTypeEnum.Constructor, member.Value, "Constructors");
-                    AddChildren(body, DocTypeEnum.Property, member.Value, "Properties");
-                    AddChildren(body, DocTypeEnum.Member, member.Value, "Methods");
+                    AddChildren(body, DocTypeEnum.Constructor, member, "Constructors");
+                    AddChildren(body, DocTypeEnum.Property, member, "Properties");
+                    AddChildren(body, DocTypeEnum.Member, member, "Methods");
                     // ToDo: Operators
                     // ToDo: Extension Methods
+                    string classFileName = Path.Combine(rootPath, EscapeFileName(qualifiedTypeName));
+                    classFileName = Path.ChangeExtension(classFileName, ".html");
+                    html.Save(classFileName);
+                    generateFileNames.Add(classFileName);
                 }
             }
-            return html;
+            GenerateIndex(rootPath, generateFileNames);
+        }
+        private void GenerateIndex(string rootPath, List<string> fileNames)
+        {
+            XElement html = new XElement("html");
+            XElement body = new XElement("body");
+            html.Add(body);
+            XElement ul = new XElement("ul");
+            body.Add(ul);
+            foreach (string fileName in fileNames)
+            {
+                XElement a = new XElement("a");
+                a.SetAttributeValue("href", Path.GetFileName(fileName));
+                a.Value = Path.GetFileNameWithoutExtension(fileName);
+                ul.Add(new XElement("li", a));
+            }
+            html.Save(Path.Combine(rootPath, "index.html"));
+        }
+        private string EscapeFileName(string name)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (char ch in name)
+            {
+                if (char.IsLetterOrDigit(ch))
+                    builder.Append(ch);
+                else
+                    builder.Append('_');
+            }
+            return builder.ToString();
         }
         private void AddChildren(XElement parentNode, DocTypeEnum docType, DocMember parentMember, string header)
         {
@@ -206,6 +238,110 @@ h1 {
                     }
                 }
             }
+        }
+        public string GetTemporaryDirectory()
+        {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+            return tempDirectory;
+        }
+        private void OnDragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.All;
+            e.Handled = true;
+        }
+        private void InputFileDrop(object sender, DragEventArgs e)
+        {
+            LogTextBox.Document.Blocks.Clear();
+            List<string> generatedFolders = new List<string>();
+            string[] fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string fileName in fileNames)
+            {
+                AddPara(string.Format("Generating Html files for {0}",
+                    Path.GetFileNameWithoutExtension(fileName)));
+                members = new Dictionary<string, DocMember>();
+                LoadDoc(fileName);
+                string rootPath = GetTemporaryDirectory();
+                generatedFolders.Add(rootPath);
+                GenerateHtml(rootPath);
+                //System.Diagnostics.Process.Start(Path.Combine(rootPath, "index.html"));
+            }
+
+            //<FlowDocument>
+            //    <Paragraph>
+            //        <Hyperlink NavigateUri="http://www.microsoft.com" RequestNavigate="Hyperlink_RequestNavigate">Microsoft</Hyperlink>
+            //    </Paragraph>
+            //</FlowDocument>
+
+            AddPara("Next steps:");
+
+            List list = new List();
+            LogTextBox.Document.Blocks.Add(list);
+            list.MarkerStyle = TextMarkerStyle.Decimal;
+            list.ListItems.Add(new ListItem(CreatePara("Open HelpNDoc, see their website: ", CreateLink("http://www.helpndoc.com", "HelpNDoc"))));
+            list.ListItems.Add(new ListItem(CreatePara("Choose Import/Folder.")));
+            ListItem listItem = new ListItem(CreatePara("Copy/paste one of the folders below."));
+            list.ListItems.Add(listItem);
+            List innerList = new List();
+            innerList.MarkerStyle = TextMarkerStyle.LowerLatin;
+            listItem.Blocks.Add(innerList);
+            foreach (string folder in generatedFolders)
+            {
+                innerList.ListItems.Add(new ListItem(CreatePara(CreateLink(Path.Combine(folder, "index.html"), folder))));
+            }
+            list.ListItems.Add(new ListItem(CreatePara("Press refresh.")));
+            list.ListItems.Add(new ListItem(CreatePara("Deselect the files you don't want, such as index.")));
+            list.ListItems.Add(new ListItem(CreatePara("Press import.")));
+            list.ListItems.Add(new ListItem(CreatePara("See the ", CreateLink("http://www.helpndoc.com", "HelpNDoc"), " manual on how to generate Html, Word, Pdf, CHM, ePub, etc.")));
+        }
+        private BlockCollection blocks;
+
+        private Hyperlink CreateLink(string url, string caption)
+        {
+            Hyperlink link = new Hyperlink();
+            link.IsEnabled = true;
+            link.Inlines.Add(caption);
+            link.NavigateUri = new Uri(url, UriKind.Absolute);
+            link.Click += link_Click;
+            return link;
+        }
+        private void AddLink(string url, string caption)
+        {
+            Paragraph para = new Paragraph();
+            blocks.Add(para);
+            Hyperlink link = CreateLink(url, caption);
+            para.Inlines.Add(link);
+        }
+        private Paragraph CreatePara(params object[] items)
+        {
+            Paragraph para = new Paragraph();
+            para.FontSize = 12;
+            para.FontWeight = FontWeights.Normal;
+            foreach (object item in items)
+            {
+                if (item is string)
+                    para.Inlines.Add((string)item);
+                else if (item is Inline)
+                    para.Inlines.Add((Inline)item);
+                else if (item is UIElement)
+                    para.Inlines.Add((UIElement)item);
+            }
+            return para;
+        }
+        private void AddPara(string text)
+        {
+            blocks.Add(CreatePara(text));
+        }
+
+        void link_Click(object sender, RoutedEventArgs e)
+        {
+            Hyperlink link = (Hyperlink)sender;
+            System.Diagnostics.Process.Start(link.NavigateUri.AbsoluteUri);
+        }
+
+        private void Website_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Jan01/Sandbagging");
         }
     }
     public enum DocTypeEnum { Type, Member, Property, Constructor }
